e525e325cef37e133187b046443ff9bf7ede0c39
gaspadat <a.sharofuddin@gmail.com>
2020-07-29 23:35:26 +0700

    :bug: fix: geolocation causing init app blocked

--- plugins/location.client.js
+++ plugins/location.client.js
@@ -1,8 +1,10 @@
 import { loadCurrentPosition } from '~/lib/core/location'
 import { SET_LATLONG } from '~/constants/storeTypes'
 
-export default async ({ store }) => {
-  const { lat, lng } = await loadCurrentPosition()
-  const latlong = `${lat},${lng}`
-  store.commit(SET_LATLONG, latlong)
+export default ({ store }) => {
+  loadCurrentPosition()
+    .then(({ lat, lng }) => {
+      const latlong = `${lat},${lng}`
+      store.commit(SET_LATLONG, latlong)
+    })
 }
